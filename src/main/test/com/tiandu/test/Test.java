e7701a897c24264878edbaa39eb3d004cf27d165
liuxinbing <liuxinbing@PC-20150724SCAB>
2016-09-02 18:04:56 +0800

    微信红包

--- src/main/test/com/tiandu/test/Test.java
+++ src/main/test/com/tiandu/test/Test.java
@@ -1,15 +1,17 @@
 package com.tiandu.test;
 
-import java.util.Random;
-
-import org.apache.commons.lang.StringUtils;
+import java.text.ParseException;
+import java.util.Iterator;
 
 import com.google.gson.Gson;
-import com.tiandu.custom.vo.ProfitInfo;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.tiandu.product.vo.AttributeOptionsVO;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
 //		System.out.println(System.currentTimeMillis());
 		/*ProfitInfo profit = new ProfitInfo();
@@ -24,11 +26,23 @@ public class Test {
     	
     	ProfitInfo prof = json.fromJson(relation, ProfitInfo.class);
     	System.out.println(prof.getOrderNo());*/
-		String str = " ";
+		/*String str = " ";
 		if(StringUtils.isNotBlank(str)){
 			System.out.println("1");
 		}else{
 			System.out.println("2");
+		}*/
+		String spes = "[{\"sname\":\"颜色\",\"soptions\":[\"ree\"]},{\"sname\":\"尺码\",\"soptions\":[\"sdfsw\"]}]";
+		AttributeOptionsVO  aos = null;
+		JsonParser parser = new JsonParser();
+		Gson gson = new Gson();
+		JsonElement element = parser.parse(spes);
+		JsonArray sarr = element.getAsJsonArray();
+		Iterator it = sarr.iterator();
+		while(it.hasNext()){
+			JsonElement e = (JsonElement)it.next();
+			aos = gson.fromJson(e, AttributeOptionsVO.class);
+			System.out.println(aos.getSname());
 		}
 	}
 
